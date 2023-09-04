310aa05528f6cb61ae853dece88e1e05bad76f8e
Unni Kurumbail <ukurumbail@wisc.edu>
2023-09-04 15:15:49 -0500

    Adds functionality to generate an IR dataset for DRIFTS, rather than TPSR, experiments.

--- tpsr_analysis/_main.py
+++ tpsr_analysis/_main.py
@@ -9,7 +9,8 @@ from _tpsr_analysis import generate_csv,analyze_tpsr
 @click.option('--analyze',default=True,help='Run analysis of TPSR data.')
 @click.option('--inert',default=28,help='M/Z of inert gas (e.g. 14, 28, 40).')
 @click.option('--ir_data_exists',default=False,help='True if you wish to collate OPUS IR data as well, False if not.')
-def main(analyze,inert,ir_data_exists):
+@click.option('--only_ir',default=False,help='Just collate IR data, not TPSR data.')
+def main(analyze,inert,ir_data_exists,only_ir):
 	print("Welcome to the TPSR analysis software for the Hermans group DRIFTS setup!")
 	print("Please ensure that you have copied the template folder into a new directory (e.g. 20230823-TPSR) and populated it with the correct files.")
 	dir_path = input("Please provide the directory (e.g. C://20230823-TPSR): ")
@@ -19,29 +20,32 @@ def main(analyze,inert,ir_data_exists):
 
 	if os.path.exists(dir_path):
 
-		#collate T and MS data
+		#collate T, MS, potentially IR data
 		print("\nGenerating dataset...")
-		df = generate_csv(dir_path,IR_data_exists=ir_data_exists)
+		df = generate_csv(dir_path,IR_data_exists=ir_data_exists,only_IR=only_ir)
 
+		if only_ir: #just an IR dataset
+			df.to_csv(dir_path+"/DRIFTS.csv",index=False)
+			print("\nSuccessully generated DRIFTS.csv.")
 
-
-
-		if ir_data_exists:
-			df.to_csv(dir_path+"/TPSR_dataset_with_IR.csv",index=False)
-			print("\nSuccessully generated TPSR_dataset_with_IR.csv.")
 		else:
-			df.to_csv(dir_path+"/TPSR_dataset.csv",index=False)
-			print("\nSuccessully generated TPSR_dataset.csv.")
 
-		if analyze: #if we wish to perform analysis:
-			df = analyze_tpsr(df,inert=inert)
-			if ir_data_exists:
-				df.to_csv(dir_path+"/TPSR_analysis_with_IR.csv",index=False)
-				print("\nSuccessully generated TPSR_analysis_with_IR.csv.")
+			if ir_data_exists: #TPSR dataset with IR
+				df.to_csv(dir_path+"/TPSR_dataset_with_IR.csv",index=False)
+				print("\nSuccessully generated TPSR_dataset_with_IR.csv.")
+			else: #TPSR dataset without IR
+				df.to_csv(dir_path+"/TPSR_dataset.csv",index=False)
+				print("\nSuccessully generated TPSR_dataset.csv.")
 
-			else:
-				df.to_csv(dir_path+"/TPSR_analysis.csv",index=False)
-				print("\nSuccessully generated TPSR_analysis.csv.")
+			if analyze: #if we wish to perform analysis:
+				df = analyze_tpsr(df,inert=inert)
+				if ir_data_exists:
+					df.to_csv(dir_path+"/TPSR_analysis_with_IR.csv",index=False)
+					print("\nSuccessully generated TPSR_analysis_with_IR.csv.")
+
+				else:
+					df.to_csv(dir_path+"/TPSR_analysis.csv",index=False)
+					print("\nSuccessully generated TPSR_analysis.csv.")
 
 	else:
 		print("Input directory does not exist! Please be sure you submit a proper directory.")
--- tpsr_analysis/_tpsr_analysis.py
+++ tpsr_analysis/_tpsr_analysis.py
@@ -7,7 +7,33 @@ from datetime import datetime
 #defined variables
 BP_T_Range = 10 #degC above T0 to go for 'bypass's
 
-def generate_csv(dir_data,IR_data_exists=False):
+def generate_csv_IR(dir_IR,dir_IR_xy):
+
+	
+	xy_files = os.listdir(dir_IR_xy)
+	timestamp_IR = [datetime.fromtimestamp(os.stat(dir_IR+f'\\{fname}').st_mtime) for fname in xy_files]
+
+	fpath = dir_IR_xy+f'\\{xy_files[0]}' #get column values from the first file
+	data = pd.read_csv(fpath,header=None,index_col=0).transpose() 
+
+	IR_wavenumbers = [str(i) for i in data.columns]
+	df_IR = pd.DataFrame(np.nan,index=timestamp_IR,columns=IR_wavenumbers) #set IR wavenumbers as column headers. Units cm^-1
+
+	for (timestamp,fname) in zip(timestamp_IR,xy_files):
+		fpath = dir_IR_xy+f'\\{fname}' 
+		data = pd.read_csv(fpath,header=None,index_col=0).transpose() #one-row df
+		df_IR.loc[timestamp,IR_wavenumbers] = data.values
+	df_IR["XY Filenames"] = xy_files
+	df_IR["Timestamp"] = df_IR.index
+	return df_IR
+
+def generate_csv(dir_data,IR_data_exists=False,only_IR=False):
+
+	dir_IR = dir_data+"\\DRIFTS"
+	dir_IR_xy = dir_data+"\\DRIFTS_xy"
+	if only_IR:
+		return generate_csv_IR(dir_IR,dir_IR_xy)
+
 	dir_T = dir_data+"\\temperature"
 	fpath_T = dir_T + "\\" + os.listdir(dir_T)[0]
 	df_T = pd.read_csv(fpath_T,skiprows=[0]) #skip first gibberish row
@@ -27,25 +53,7 @@ def generate_csv(dir_data,IR_data_exists=False):
 
 	#3. Generate df from the IR data if it exists
 	if IR_data_exists:
-		dir_IR = dir_data+"\\DRIFTS"
-		dir_IR_xy = dir_data+"\\DRIFTS_xy"
-
-		
-		xy_files = os.listdir(dir_IR_xy)
-		timestamp_IR = [datetime.fromtimestamp(os.stat(dir_IR+f'\\{fname}').st_mtime) for fname in xy_files]
-
-		fpath = dir_IR_xy+f'\\{xy_files[0]}' #get column values from the first file
-		data = pd.read_csv(fpath,header=None,index_col=0).transpose() 
-
-		IR_wavenumbers = [str(i) for i in data.columns]
-		df_IR = pd.DataFrame(np.nan,index=timestamp_IR,columns=IR_wavenumbers) #set IR wavenumbers as column headers. Units cm^-1
-
-		for (timestamp,fname) in zip(timestamp_IR,xy_files):
-			fpath = dir_IR_xy+f'\\{fname}' 
-			data = pd.read_csv(fpath,header=None,index_col=0).transpose() #one-row df
-			df_IR.loc[timestamp,IR_wavenumbers] = data.values
-		df_IR["XY Filenames"] = xy_files
-		df_IR["Timestamp"] = df_IR.index
+		df_IR = generate_df_IR(dir_IR,dir_IR_xy)
 
 	#4. Combine dfs
 	if IR_data_exists: #generate one datapt per IR spectrum
